980c10e53d57a505bec96087e905c1b543a4f837
Yulia Sadovskaya <yulsonn@gmail.com>
2015-11-18 21:36:18 +0300

    added Fabric plugin

--- app/src/main/java/ru/loftschool/bashclient/ZabolbaliApplication.java
+++ app/src/main/java/ru/loftschool/bashclient/ZabolbaliApplication.java
@@ -1,12 +1,15 @@
 package ru.loftschool.bashclient;
 
 import com.activeandroid.ActiveAndroid;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class ZabolbaliApplication extends com.activeandroid.app.Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         ActiveAndroid.initialize(this);
     }
 }

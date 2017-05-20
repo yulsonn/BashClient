bee5848d7fb3752617235db5a0955dbccdde62ac
Juliya Sadovskaya <jsadovskaya@4xxi.com>
2017-05-21 00:02:21 +0300

    fixed base_url

--- app/src/main/java/ru/loftschool/bashclient/rest/RestGeneralParams.java
+++ app/src/main/java/ru/loftschool/bashclient/rest/RestGeneralParams.java
@@ -2,7 +2,7 @@ package ru.loftschool.bashclient.rest;
 
 public class RestGeneralParams {
 
-    public static final String BASE_URL = "http://www.umori.li";
+    public static final String BASE_URL = "http://umorili.herokuapp.com";
     public static final String SITE = "zadolba.li";
     public static final String NAME = "zadolbali";
     public static final int POSTS_QTY = 50;
